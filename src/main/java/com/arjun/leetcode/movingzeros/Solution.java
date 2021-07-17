44de09a3cc3c792724482b907a1b99af8045c451
Arjun Sahasranam <arjunsahas@gmail.com>
2021-07-17 17:30:15 +0530

    moving-zeros

---
+++ src/main/java/com/arjun/leetcode/movingzeros/Solution.java
@@ -0,0 +1,16 @@
+package com.arjun.leetcode.movingzeros;
+
+public class Solution {
+        public void moveZeroes(int[] nums) {
+            int count = 0;
+            for( int i = 0; i < nums.length; i ++) {
+               if(nums[i] !=0) {
+                   nums[count] = nums[i];
+                   count++;
+               }
+            }
+           for ( int i = count; i < nums.length; i ++) {
+               nums[i] = 0;
+           }
+        }
+    }

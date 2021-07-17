df64b1524d34d85e38e8bfcb1202c5e9a07aea6c
Arjun Sahasranam <arjunsahas@gmail.com>
2021-07-17 17:09:13 +0530

    buy-sell

---
+++ src/main/java/com/arjun/leetcode/buysellstock/Solution.java
@@ -0,0 +1,22 @@
+package com.arjun.leetcode.buysellstock;
+
+class Solution {
+    private static int maxProfit;
+
+    public int maxProfit(int[] prices) {
+        int leastCost = -1;
+        int profit = -1;
+        for (int day = 0; day < prices.length; day++) {
+            if (prices[day] < leastCost || leastCost == -1) {
+                leastCost = prices[day];
+            }
+            profit = Math.max(profit, prices[day] - leastCost);
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        maxProfit = new Solution().maxProfit(new int[] { 7, 6, 4, 3, 1 });
+        System.out.println(maxProfit);
+    }
+}
